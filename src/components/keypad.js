4e3744dfc0a31d70853e1ca62a33de6607afa544
itsyugam <yugamsachdeva100@gmail.com>
2022-07-07 20:07:31 +0530

    fixed responsiveness

--- src/components/keypad.js
+++ src/components/keypad.js
@@ -12,7 +12,7 @@ var Keypad = () => {
     // functions
     const handleOnChange = (e) => {
         setText(e.target.value);
-      }
+    }
 
     const addOpera = (e) => {
         setText(text + e.target.value)
@@ -36,19 +36,24 @@ var Keypad = () => {
     return (
         <>
             <div className='container my-5'>
-                <TextForm error={error} text={text} handleOnChange={handleOnChange}/>
+                <TextForm error={error} text={text} handleOnChange={handleOnChange} />
                 <div className="my-2">
-                    <button type="button" className="btn btn-outline-dark me-3" value="1" onClick={addOpera}>1</button>
-                    <button type="button" className="btn btn-outline-dark mx-3" value="2" onClick={addOpera}>2</button>
-                    <button type="button" className="btn btn-outline-dark mx-3" value="3" onClick={addOpera}>3</button>
-                    <button type="button" className="btn btn-outline-dark mx-3" value="4" onClick={addOpera}>4</button>
-                    <button type="button" className="btn btn-outline-dark mx-3" value="5" onClick={addOpera}>5</button>
-                    <button type="button" className="btn btn-outline-dark mx-3" value="6" onClick={addOpera}>6</button>
-                    <button type="button" className="btn btn-outline-dark mx-3" value="7" onClick={addOpera}>7</button>
-                    <button type="button" className="btn btn-outline-dark mx-3" value="8" onClick={addOpera}>8</button>
-                    <button type="button" className="btn btn-outline-dark mx-3" value="9" onClick={addOpera}>9</button>
-                    <button type="button" className="btn btn-outline-dark mx-3" value="0" onClick={addOpera}>0</button>
-
+                    <div className="row">
+                        <div className="my-2 pe-0 col-md-auto col-sm-12">
+                            <button type="button" className="btn btn-outline-dark me-3" value="1" onClick={addOpera}>1</button>
+                            <button type="button" className="btn btn-outline-dark mx-3" value="2" onClick={addOpera}>2</button>
+                            <button type="button" className="btn btn-outline-dark mx-3" value="3" onClick={addOpera}>3</button>
+                            <button type="button" className="btn btn-outline-dark mx-3" value="4" onClick={addOpera}>4</button>
+                            <button type="button" className="btn btn-outline-dark mx-3" value="5" onClick={addOpera}>5</button>
+                        </div>
+                        <div className="my-2 p-md-0 col-md-auto col-sm-12">
+                            <button type="button" className="btn btn-outline-dark ms-md-3 me-3" value="6" onClick={addOpera}>6</button>
+                            <button type="button" className="btn btn-outline-dark mx-3" value="7" onClick={addOpera}>7</button>
+                            <button type="button" className="btn btn-outline-dark mx-3" value="8" onClick={addOpera}>8</button>
+                            <button type="button" className="btn btn-outline-dark mx-3" value="9" onClick={addOpera}>9</button>
+                            <button type="button" className="btn btn-outline-dark mx-3" value="0" onClick={addOpera}>0</button>
+                        </div>
+                    </div>
                     <div className="my-2">
                         <button type="button" className="btn btn-success me-3" value="+" onClick={addOpera}>+</button>
                         <button type="button" className="btn btn-danger mx-3" value="-" onClick={addOpera}>-</button>
